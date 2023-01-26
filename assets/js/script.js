0f6b876cf9c21021f92f8aa93aff66b98f5734e8
Giulia Polenta <giuliapolenta@gmail.com>
2023-01-26 14:56:52 +0100

    Fix bug in anchor tag

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,7 @@
 const buttonLeft = document.querySelector(".arrow-left");
 const buttonRight = document.querySelector(".arrow-right");
 const scrollContainer = document.querySelector(".grid-container");
-const buttonOpenPopup = document.querySelectorAll(".menu-link a, .--txt-link, .--open-popup a");
+const buttonOpenPopup = document.querySelectorAll(".menu-link a, span.--txt-link, .--open-popup a");
 const elementPopup = document.querySelectorAll(".popup-container");
 const buttonClosePopup = document.querySelectorAll(".popup-close-button");
 const header = document.querySelector(".header");
